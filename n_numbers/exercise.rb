43de2b5d7c3a539d44a7103916cc150df0d081df
szaman76 <m.szymanski1995@gmail.com>
2022-11-04 13:08:16 +0100

    trzy

--- n_numbers/exercise.rb
+++ n_numbers/exercise.rb
@@ -175,22 +175,67 @@ def consecutive_collapse(array)
         if (array[i] - array[i+1] == 1) || (array[i] - array[i+1] == -1)
             array = array[0...i] + array[i+2..-1]
             i = 0
+        else
+            i += 1
         end
 
-        if i == array.length || array.length <= 2
+        if i == array.length - 1 || array.length < 2
             iksde = false
         end
-        i += 1
     end
     array
 end
+def greater(elem,nextt)
+    count = 0
+    i = 1
+    result = 0
+    while count < nextt
+        if is_prime?(elem + i)
+            result = elem + i
+            count += 1
+        end
+        i += 1
+    end
+    result
+end
+
+def lesser(elem,nextt)
+    #byebug
+    count = 0
+    i = 1
+    result = 0
+    while count > nextt
+        if is_prime?(elem - i)
+            result = elem - i
+            count -= 1
+        elsif elem - i < 2
+            return nil
+        end
+        i += 1
+    end
+    result
+end
+
+def pretentious_primes(array, num)
+    arr = []
+    array.each do |ele|
+        if num > 0
+            arr << greater(ele,num)
+        elsif num < 0
+            arr << lesser(ele,num)
+        end
+    end
+    arr
+end
 
 
-#p consecutive_collapse([3, 4, 1])                     # [1]
-#p consecutive_collapse([1, 4, 3, 7])                  # [1, 7]
-#p consecutive_collapse([9, 8, 2])                     # [2]
-#p consecutive_collapse([9, 8, 4, 5, 6])               # [6]
- p consecutive_collapse([1, 9, 8, 6, 4, 5, 7, 9, 2])   # [1, 9, 2]
-# p consecutive_collapse([3, 5, 6, 2, 1])               # [1]
-# p consecutive_collapse([5, 7, 9, 9])                  # [5, 7, 9, 9]
-# p consecutive_collapse([13, 11, 12, 12])              # []
+ p pretentious_primes([4, 15, 7], 1)           # [5, 17, 11]
+ p pretentious_primes([4, 15, 7], 2)           # [7, 19, 13]
+ p pretentious_primes([12, 11, 14, 15, 7], 1)  # [13, 13, 17, 17, 11]
+p pretentious_primes([12, 11, 14, 15, 7], 3)  # [19, 19, 23, 23, 17]
+ p pretentious_primes([4, 15, 7], -1)          # [3, 13, 5]
+ p pretentious_primes([4, 15, 7], -2)          # [2, 11, 3]
+ p pretentious_primes([2, 11, 21], -1)         # [nil, 7, 19]
+ p pretentious_primes([32, 5, 11], -3)         # [23, nil, 3]
+ p pretentious_primes([32, 5, 11], -4)         # [19, nil, 2]
+ p pretentious_primes([32, 5, 11], -5)         # [17, nil, nil]

58669ee4d468511df702235f1d5960a32ab3b02c
szaman76 <m.szymanski1995@gmail.com>
2022-11-16 12:59:16 +0100

    bieg

--- meddling_medley/exercise.rb
+++ meddling_medley/exercise.rb
@@ -73,6 +73,15 @@ def uncompress(string)
     arr.join("")
 end
 
-p uncompress('a2b4c1') # 'aabbbbc'
-p uncompress('b1o2t1') # 'boot'
-p uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+def conjunct_select(array, *procs)
+    
+
+end
+
+is_positive = Proc.new { |n| n > 0 }
+is_odd = Proc.new { |n| n.odd? }
+less_than_ten = Proc.new { |n| n < 10 }
+
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]

6499aa6f859d22294fc531797eb0efff29d0e594
szaman76 <m.szymanski1995@gmail.com>
2023-06-20 20:08:20 +0200

    finished project - Implementing support for multiple lists

--- todo_board/list.rb
+++ todo_board/list.rb
@@ -43,7 +43,7 @@ class List
 
     def print
         p "************************************"
-        p "            Groceries               "
+        p "            #{label.upcase}               "
         p "************************************"
         p "Index | Item           | Deadline|  Done"
         p "************************************"
--- todo_board/todo_board.rb
+++ todo_board/todo_board.rb
@@ -3,48 +3,60 @@ require "byebug"
 class TodoBoard
 
 
-    def initialize(label)
-        @list = List.new(label)
+    def initialize
+        @list = Hash.new
     end
 
     def get_command
         print "\nEnter a command: "
-        cmd, *args = gets.chomp.split(' ')
+        cmd, label, *args = gets.chomp.split(' ')
 
         case cmd
-        when 'mktodo'
-            @list.add_item(*args)
+        when 'mklist' # make a new list with the given label
+            @list[label] = List.new(label)
             true
-        when 'up'
+        when 'mktodo' # add a new item to the specified list with the given information
+            @list[label].add_item(*args)
+            true
+        when 'up' #move the specified item higher on the given list
             arr = args.map {|item| item = item.to_i}
             @list.up(arr[0],*arr[1])
             true
-        when 'down'
+        when 'down' #move the specified item higher on the given list
             arr = args.map {|item| item = item.to_i}
             @list.down(arr[0],*arr[1])
             true
-        when 'swap'
+        when 'ls' # print the labels of all lists
+            @list.each_key {|key| p key.to_sym}
+            true
+        when 'swap' #swap the positions of the specified items on the given list
             @list.swap(args[0].to_i, args[1].to_i)
             true
-        when 'sort'
+        when 'sort' #sort the given list by deadline
             @list.sort_by_date!
             true
-        when 'priority'
+        when 'showall'
+            @list.each_key {|key| @list[key].print}
+            true
+        when 'priority' #print the all information for the item at the top of the given list
             @list.print_prority
             true
-        when 'print'
+        when 'print'#print all items of the given list if index is not provided
             if args.empty?
-                @list.print
-            else
-                @list.print_full_item(args[0].to_i)
+                @list[label].print
+            else#print the specific item of the given list if index is provided
+                @list[label].print_full_item(args[0].to_i)
             end
             true
-        when 'purge'
-            @list.purge
-        when 'toggle'
-            @list.toggle_item(args[0].to_i)
-        when 'rm'
-            @list.remove_item(args[0].to_i)
+        when 'purge' #remove all "done" items on the given list
+            @list[label].purge
+            true
+        when 'toggle' #toggle "done" for the specified item on the given list
+            @list[label].toggle_item(args[0].to_i)
+            true
+        when 'rm' # remove the specified item on the given list
+            @list[label].remove_item(args[0].to_i)
+            true
         when 'quit'
             return false
         else
@@ -60,3 +72,6 @@ class TodoBoard
 
 
 end
+
+my_board = TodoBoard.new
+my_board.run

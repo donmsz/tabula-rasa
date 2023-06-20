d3bd3b794ac7d9a87a4a2b42b00526ba5f19cdcb
szaman76 <m.szymanski1995@gmail.com>
2023-06-20 13:44:28 +0200

    Update item.rb

--- todo_board/item.rb
+++ todo_board/item.rb
@@ -15,11 +15,12 @@ class Item
 
 
     def initialize(title, deadline, description)
-        @title =title
+        raise "raises error due to invalid date" if !Item.valid_date?(deadline)
+        @title = title
         @deadline = deadline
         @description = description
 
-        
+
 
 
     end
@@ -30,8 +31,14 @@ class Item
 end
 
 
-p Item.valid_date?('2019-10-25') # true
-p Item.valid_date?('1912-06-23') # true
-p Item.valid_date?('2018-13-20') # false
-p Item.valid_date?('2018-12-32') # false
-p Item.valid_date?('10-25-2019') # false
+p Item.new('Fix login page', '2019-10-25', 'The page loads too slow.')
+p Item.new(
+    'Buy Cheese',
+    '2019-10-21',
+    'We require American, Swiss, Feta, and Mozzarella cheese for the Happy hour!'
+)
+p Item.new(
+    'Fix checkout page',
+    '10-25-2019',
+    'The font is too small.'
+)
